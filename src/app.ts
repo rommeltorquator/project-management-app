ef108649a1ba372a2eb65c9d592dc94944b40ebf
Rommel Torquator <rommel.torquator@ewave.com>
2024-09-18 18:29:05 +0800

    Initial setup

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Database Connection
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
