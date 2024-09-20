48bc8c9c28cad531adbf88cba927ca3bd453db35
Rommel Torquator <rommel.torquator@ewave.com>
2024-09-20 14:14:02 +0800

    Added an async middleware

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,21 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+
+// routes
+import authRoutes from './routes/authRouter';
+import projectRoutes from './routes/projectsRouter';
+import taskRoutes from './routes/tasksRouter';
+
+import { errorHandler } from './middleware/errorHandler';
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
@@ -16,6 +25,13 @@ mongoose
   .connect(process.env.MONGO_URI as string)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error(err));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
+
+app.use(errorHandler); // should be the last middleware
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
